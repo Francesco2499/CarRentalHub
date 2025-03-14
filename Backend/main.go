fb0165caff2746795934b5b904b33419aef7fec2
Piero Galatà <pgalata@xeniaprogetti.it>
2025-03-14 11:43:53 +0100

    Updated user model and r

--- Backend/main.go
+++ Backend/main.go
@@ -4,7 +4,33 @@ import (
 	"Backend/config"
 	"Backend/routes"
 	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	// Crea la cartella logs se non esiste
+	err := os.MkdirAll("logs", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating logs folder:", err)
+	}
+
+	// Apre (o crea) il file di log
+	logFile, err := os.OpenFile("logs/carrentalhub.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Error opening log file:", err)
+	} else {
+		// Scrivi log sia su stdout (console) che su file
+		multiWriter := io.MultiWriter(os.Stdout, logFile)
+		log.SetOutput(multiWriter)
+
+		gin.DefaultWriter = multiWriter
+		gin.DefaultErrorWriter = multiWriter
+	}
+}
 
 func main() {
 	db, err := config.InitDB()
@@ -15,5 +41,11 @@ func main() {
 	defer db.Close()
 
 	r := routes.SetupRoutes()
-	r.Run(":8085")
+	r.SetTrustedProxies(nil)
+
+	//r.Run(":8085")
+	//log.Println("Server started on port 8085")
+	if err := r.Run(":8085"); err != nil {
+		log.Println("Server startup error:", err)
+	}
 }
--- Backend/models/user.go
+++ Backend/models/user.go
@@ -5,5 +5,6 @@ type User struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Region   string `json: "region"`
 	Role     string `json:"role"` // "admin" o "customer"
 }
